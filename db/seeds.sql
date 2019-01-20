f9508b685db4d6a5bb6a7616390367bb49013d2e
rachelsue <wells.rachelsue@gmail.com>
2019-01-20 14:28:21 -0600

    initial setup, start of code, complete db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, devoured) VALUES ('Royale with Cheese', false);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Cowboy Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('The Juggernaut', false);
+INSERT INTO burgers (name, devoured) VALUES ('Buffalo Burger', false);
